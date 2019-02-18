76c798134d7e22e91b7808fcbced46e6c58330cd
Carbon Eternity <pa03622789@gmail.com>
2019-02-18 22:21:36 +0200

    add graphic without buttons

---
+++ src/Main.py
@@ -0,0 +1,44 @@
+from tkinter import *
+import math
+
+root =Tk()
+root.title('Построение графика функции sin(x)')
+root.geometry('1320x640')
+
+canvas = Canvas(root, width=1040, height=640, bg='#d9d9ea')  #002
+canvas.pack(side='right')
+
+#линии сетки по вертикали
+for y in range(21):
+    k=50*y
+    canvas.create_line(20+k, 620, 20+k, 20, width=0.5, fill='#191938')
+
+
+#линии сетки по горизонтали
+for x in range(13):
+    k=50*x
+    canvas.create_line(20, 20+k, 1020, 20+k, width=0.5, fill='#191938')
+
+#оси координат
+canvas.create_line(20, 20, 20, 620, width=2, arrow=FIRST, fill='black') #ось у
+canvas.create_line(10, 320, 1020, 320, width=2, arrow=LAST, fill='black') #ось х
+
+canvas.create_text(20, 10, text='300', fill='black')
+canvas.create_text(20, 630, text='-300', fill='black')
+canvas.create_text(10, 310, text='0', fill='black')
+canvas.create_text(1030, 310, text='1000', fill='black')
+
+w = 0.0209 #циклич частота
+phi =10    #смещение графика по х
+A =200     #амплитуда
+dy= 310    #смещение графика по у
+
+#список где хранится значение функции
+xy =[]
+for x in range(1000):
+    y=math.sin(x*w)
+    xy.append(x+phi)
+    xy.append(y*A+dy)
+sin_line=canvas.create_line(xy, width=2, fill='blue')
+
+root.mainloop()
